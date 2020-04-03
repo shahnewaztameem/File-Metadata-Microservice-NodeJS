ad4ecd10ac067d08267cebc33d89c0c524dcb790
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-04-03 16:35:27 +0600

    multer added and file info returned as json

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
-
+var multer = require('multer');
+var upload = multer({dest: '/uploads'});
 var app = express();
 
 app.use(cors());
@@ -16,6 +17,16 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+
+
+app.post('/api/fileanalyse',upload.single('upfile'),  (req, res, next) => {
+  var name = req.file.originalname;
+  var type = req.file.mimetype;
+  var size = req.file.size;
+  res.json({name: name, type: type, size: size});
+  next();
 });
 
 app.listen(process.env.PORT || 3000, function () {
